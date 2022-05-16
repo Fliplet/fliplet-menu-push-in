847a324a8e0210b7201adaae76278941c116454a
OleksandrLavrynenko <86256215+OleksandrLavrynenko@users.noreply.github.com>
2022-05-16 11:01:24 +0300

    OD-573 [Feature] Added translation to Widget-MenuPushIn (#25)
    * [ADD] add translation
    
    * [FIX] fix name
    
    * [ADD] add translation
    
    * [FIX] fix translation
    
    * [FIX] fix lint
    
    * [FIX]
    
    * [FIX] fixed spacing
    
    * [update] update lint

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 var $menuElement = $('[data-name="Push in"]');
 var menuInstanceId = $menuElement.data('id');
 
+$($menuElement).translate();
+
 if (menuInstanceId) {
   init();
 }
@@ -9,14 +11,14 @@ function init() {
   var data = Fliplet.Widget.getData(menuInstanceId) || {};
   var lastScrollTop = 0;
 
-  Fliplet.Hooks.on('addExitAppMenuLink', function () {
+  Fliplet.Hooks.on('addExitAppMenuLink', function() {
     var $exitButton = $([
       '<li class="linked with-icon" data-fl-exit-app>',
-        '<div class="fl-menu-icon">',
-          '<i class="fa fa-fw fa-sign-out"></i>',
-        '</div>',
-        '<i class="fa fa-angle-right linked-icon" aria-hidden="true"></i>',
-        '<span class="internal-link buttonControl">Exit</span>',
+      '<div class="fl-menu-icon">',
+      '<i class="fa fa-fw fa-sign-out"></i>',
+      '</div>',
+      '<i class="fa fa-angle-right linked-icon" aria-hidden="true"></i>',
+      '<span class="internal-link buttonControl">' + T('widgets.menu.pushIn.actions.exit') + '</span>',
       '</li>'
     ].join(''));
 
@@ -39,15 +41,17 @@ function init() {
   }
 
   if (data.hide) {
-    $(window).scroll(function(){
+    $(window).scroll(function() {
       var st = $(this).scrollTop();
-      if (st > lastScrollTop){
+
+      if (st > lastScrollTop) {
         // downscroll code
         $('body').addClass('fl-top-menu-hidden');
       } else {
         // upscroll code
         $('body').removeClass('fl-top-menu-hidden');
       }
+
       lastScrollTop = st;
     });
   }
@@ -91,7 +95,7 @@ function init() {
     }
 
     var $body = $('body');
-    
+
     $body.addClass('has-push-menu');
     $('html').addClass('has-push-menu');
 
